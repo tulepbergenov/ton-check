8a7fe00b20247bc24d8c68c8477d81940297ee40
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-07-24 11:23:01 +0500

    feat: add error handling

--- main.ts
+++ main.ts
@@ -6,29 +6,39 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: resolve(__dirname, ".env.local") });
 
-const mnemonic = process.env.MNEMONIC_PHRASE as string;
+const mnemonic = process.env.MNEMONIC_PHRASE;
 
 const main = async () => {
-  const key = await mnemonicToWalletKey(mnemonic.split(" "));
+  try {
+    if (!mnemonic) {
+      throw new Error(
+        "MNEMONIC_PHRASE is not defined in the environment variables"
+      );
+    }
 
-  const wallet = WalletContractV4.create({
-    publicKey: key.publicKey,
-    workchain: 0,
-  });
+    const key = await mnemonicToWalletKey(mnemonic.split(" "));
 
-  const endpoint = await getHttpEndpoint({
-    network: "testnet",
-  });
+    const wallet = WalletContractV4.create({
+      publicKey: key.publicKey,
+      workchain: 0,
+    });
 
-  const client = new TonClient({
-    endpoint,
-  });
+    const endpoint = await getHttpEndpoint({
+      network: "testnet",
+    });
 
-  if (!(await client.isContractDeployed(wallet.address))) {
-    console.log("Wallet is not deployed");
+    const client = new TonClient({
+      endpoint,
+    });
+
+    if (!(await client.isContractDeployed(wallet.address))) {
+      console.log("Wallet is not deployed");
+    } else {
+      console.log("Wallet is deployed");
+    }
+  } catch (error) {
+    console.error(error);
   }
-
-  console.log("Wallet is deployed");
 };
 
 main();
