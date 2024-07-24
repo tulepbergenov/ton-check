20b2910e77f1a6cf0e2e72deffcf63e489f76cf0
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-07-24 12:02:57 +0500

    feat: add getting nft prize

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
 import { getHttpEndpoint } from "@orbs-network/ton-access";
-import { fromNano, TonClient, WalletContractV4 } from "ton";
+import { fromNano, internal, TonClient, WalletContractV4 } from "ton";
 import { mnemonicToWalletKey } from "ton-crypto";
 import { resolve } from "path";
 import dotenv from "dotenv";
 import chalk from "chalk";
+import { sleep } from "./lib";
 
 dotenv.config({ path: resolve(__dirname, ".env.local") });
 
-const mnemonic = process.env.MNEMONIC_PHRASE;
+const MNEMONIC_PHRASE = process.env.MNEMONIC_PHRASE;
+const TEST_SENDING_ADDRESS = process.env.TEST_SENDING_ADDRESS;
 
 const main = async () => {
   try {
-    if (!mnemonic) {
+    if (!MNEMONIC_PHRASE || !TEST_SENDING_ADDRESS) {
       throw new Error(
-        "MNEMONIC_PHRASE is not defined in the environment variables"
+        "MNEMONIC_PHRASE or TEST_SENDING_ADDRESS is not defined in the environment variables"
       );
     }
 
-    const key = await mnemonicToWalletKey(mnemonic.split(" "));
+    const key = await mnemonicToWalletKey(MNEMONIC_PHRASE.split(" "));
 
     const wallet = WalletContractV4.create({
       publicKey: key.publicKey,
@@ -42,8 +44,38 @@ const main = async () => {
     const balance = await client.getBalance(wallet.address);
 
     console.log(chalk.blue("Wallet"), fromNano(balance));
+
+    const walletContract = client.open(wallet);
+    const seqno = await walletContract.getSeqno();
+
+    await walletContract.sendTransfer({
+      secretKey: key.secretKey,
+      seqno,
+      messages: [
+        internal({
+          to: TEST_SENDING_ADDRESS,
+          value: "0.05",
+          body: "Hello",
+          bounce: false,
+        }),
+      ],
+    });
+
+    let currentSegno = seqno;
+
+    while (currentSegno === seqno) {
+      console.log(
+        chalk.yellow("Waiting for the transaction to be processed...")
+      );
+
+      await sleep(1500);
+
+      currentSegno = await walletContract.getSeqno();
+    }
+
+    console.log(chalk.green("Transaction is processed"));
   } catch (error) {
-    console.error(error);
+    console.error(chalk.red(error));
   }
 };
 
